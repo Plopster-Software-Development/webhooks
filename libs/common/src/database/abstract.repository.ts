388b3b1ada1b7ec697fc5b692e3955b814e0cf9d
Nicodav28 <esteveznicolas0@gmail.com>
2024-08-05 10:44:22 -0500

    🔧 (Dockerfile): Eliminar el flag --frozen-lockfile de los comandos yarn install ✨ (whatsapp.module.ts): Añadir envFilePath al ConfigModule para cargar variables de entorno desde un archivo .env 📝 (abstract.repository.ts): Añadir generación de UUID y log en la creación de documentos Se eliminó el flag --frozen-lockfile de los comandos yarn install en el Dockerfile para permitir la instalación de paquetes sin restricciones. En whatsapp.module.ts, se añadió envFilePath al ConfigModule para cargar variables de entorno desde un archivo .env, lo que facilita la gestión de configuraciones. En abstract.repository.ts, se añadió la generación de UUID y un log al crear documentos para mejorar la trazabilidad y el manejo de datos en la base de datos.

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -9,9 +9,12 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   constructor(protected readonly model: Model<TDocument>) {}
 
   async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const id = uuid.v4();
+    console.log('UUID =>', id);
+
     const createdDocument = new this.model({
       ...document,
-      _id: uuid.v4(),
+      _id: id,
     });
 
     return (await createdDocument.save()).toJSON() as unknown as TDocument;
